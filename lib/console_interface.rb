3c1d5e0bcf80e7a7fe4b8855ffb76818033e1586
Alexey <alexx10@mail.ru>
2020-10-08 07:28:18 +0300

    Remove spaces, correct readme file

--- lib/console_interface.rb
+++ lib/console_interface.rb
@@ -1,8 +1,8 @@
 class ConsoleInterface
   FIGURES =
-      Dir[__dir__ + '/../data/figures/*.txt'].
-      sort.
-      map { |file_name| File.read(file_name) }
+    Dir["#{__dir__}/data/figures/*.txt"]
+    .sort
+    .map { |file_name| File.read(file_name) }
 
   def initialize(game)
     @game = game
@@ -18,36 +18,36 @@ class ConsoleInterface
     END
 
     if @game.won?
-      puts "Поздравляем, вы выиграли!"
+      puts 'Поздравляем, вы выиграли!'
     elsif @game.lost?
       puts "Вы проиграли, загаданное слово: #{@game.word}"
     end
   end
 
   def figure
-    return FIGURES[@game.errors_made]
+    FIGURES[@game.errors_made]
   end
 
   def word_to_show
     result =
       @game.letters_to_guess.map do |letter|
-        if letter == nil
-          "__"
+        if letter.nil?
+          '__'
         else
           letter
         end
       end
 
-    return result.join(' ')
+    result.join(' ')
   end
 
   def errors_to_show
-    return @game.errors.join(', ')
+    @game.errors.join(', ')
   end
 
   def get_input
-    print "Введите следующую букву: "
+    print 'Введите следующую букву: '
     letter = gets[0].upcase
-    return letter
+    letter
   end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -7,59 +7,51 @@ class Game
   end
 
   def errors
-    return @user_guesses - normalized_letters
+    @user_guesses - normalized_letters
   end
 
   def errors_made
-    return errors.length
+    errors.length
   end
 
   def errors_allowed
-    return TOTAL_ERRORS_ALLOWED - errors_made
+    TOTAL_ERRORS_ALLOWED - errors_made
   end
 
   def letters_to_guess
-    result =
       @letters.map do |letter|
-        if @user_guesses.include?(normalize_letter(letter))
-          letter
-        else
-          nil
-        end
+        letter if @user_guesses.include?(normalize_letter(letter))
       end
-
-    return result
   end
 
   def lost?
-    return errors_allowed == 0
+    errors_allowed.zero?
   end
 
   def normalize_letter(letter)
-    return "Е" if letter == "Ё"
-    return "И" if letter == "Й"
+    return 'Е' if letter == 'Ё'
+    return 'И' if letter == 'Й'
+
     letter
   end
 
   def normalized_letters
-    @letters.map{ |x| normalize_letter(x) }
+    @letters.map { |x| normalize_letter(x) }
   end
 
   def over?
-    return won? || lost?
+    won? || lost?
   end
 
   def play!(letter)
-    if !over? && !@user_guesses.include?(normalize_letter(letter))
-      @user_guesses << normalize_letter(letter)
-    end
+    @user_guesses << normalize_letter(letter) if !over? && !@user_guesses.include?(normalize_letter(letter))
   end
 
   def won?
-    return (normalized_letters - @user_guesses).empty?
+    (normalized_letters - @user_guesses).empty?
   end
 
   def word
-    return @letters.join
+    @letters.join
   end
 end
