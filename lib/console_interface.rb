eb4303db92dd0c5aa6e4f01893f707648490380b
Alexey <alexx10@mail.ru>
2020-12-04 07:29:40 +0300

    add gem colorize and Gemfile

--- lib/console_interface.rb
+++ lib/console_interface.rb
@@ -1,6 +1,8 @@
+require 'colorize'
+
 class ConsoleInterface
   FIGURES =
-    Dir["#{__dir__}/data/figures/*.txt"]
+    Dir["#{__dir__}/../data/figures/*.txt"]
     .sort
     .map { |file_name| File.read(file_name) }
 
@@ -9,13 +11,10 @@ class ConsoleInterface
   end
 
   def print_out
-    puts <<~END
-      Слово: #{word_to_show}
-      #{figure}
-      Ошибки (#{@game.errors_made}): #{errors_to_show}
-      У вас осталось ошибок: #{@game.errors_allowed}
-
-    END
+    puts "Слово: #{word_to_show}".colorize(:blue)
+    puts figure.colorize(:yellow)
+    puts "Ошибки (#{@game.errors_made}): #{errors_to_show}".colorize(:red)
+    puts "У вас осталось ошибок: #{@game.errors_allowed}"
 
     if @game.won?
       puts 'Поздравляем, вы выиграли!'
